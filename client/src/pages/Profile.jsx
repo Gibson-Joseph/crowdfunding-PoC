1142e584cbc111ed7154fed691a1f7c03c81c6c2
Gibosn <gibsonjoseph890@gmail.com>
2023-02-13 15:02:38 +0530

    add some CSS

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,7 +13,7 @@ const Profile = () => {
   return (
     <>
       <h1 className='flex justify-center w-full font-epilogue text-white text-[18px]'>Wallet Connected!</h1>
-      <div className='flex py-2 w-full justify-center mb-4'><h1 className='font-epilogue font-semibold text-[16px] text-white text-left'>Wallet Address :</h1> <span className='font-epilogue font-normal text-[#818183] ml-2'>{address}</span></div>
+      <div className='flex py-2 w-full justify-center mb-4'><h1 className='font-epilogue font-semibold text-[16px] text-white text-left whitespace-nowrap'>Wallet Address :</h1> <span className='font-epilogue font-normal text-[#818183] ml-2 truncate'>{address}</span></div>
       <DisplayCampaigns title="All Campaigns" />
     </>
   )
