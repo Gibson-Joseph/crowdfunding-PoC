74f1875477e0698ba79929557eba2d2ac3281841
Gibosn <gibsonjoseph890@gmail.com>
2023-02-14 16:51:37 +0530

    add donators icon

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -3,7 +3,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useStateContext } from "../context";
 import { CustomButton, CountBox, Loader } from "../components";
 import { calculateBarPercentage, daysLeft } from "../utils";
-import { thirdweb } from "../assets";
+import { thirdweb, multiDonator } from "../assets";
 
 const CampaignDetails = () => {
   const { state } = useLocation();
@@ -35,6 +35,7 @@ const CampaignDetails = () => {
       {isLoading && <Loader />}
       <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
         <div className="flex-1 flex-col">
+          <h2 className="font-epilogue text-white text-[25px] font-normal mb-2 uppercase">{state.title}</h2>
           <img
             src={state.image}
             alt="fund"
@@ -99,9 +100,14 @@ const CampaignDetails = () => {
           <div>
             <div className="mt-[20px] flex flex-col gap-4">
               <div className="flex justify-between items-center gap-4">
-                <h2 className="font-epilogue font-semibold text-[18px] text-white uppercase">
-                  Donators
-                </h2>
+                <div className="flex justify-center items-center">
+                  <div className="w-[42px] h-[42px] flex items-center justify-center rounded-full bg-[#2c2f3d]">
+                    <img src={multiDonator} alt="donators" className="w-[60%] h-[60%] object-contain " />
+                  </div>
+                  <h2 className="font-epilogue font-semibold text-[18px] text-white uppercase ml-2">
+                    Donators
+                  </h2>
+                </div>
                 <h2 className="font-epilogue font-semibold text-[18px] text-white uppercase">
                   ETH Amounts
                 </h2>
